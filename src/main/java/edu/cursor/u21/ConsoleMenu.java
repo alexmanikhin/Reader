1e8857c6eac6594607bcc0755045f47c2b69eb63
Alexandr Manikhin <alexandrmanikhin@Alexandrs-MacBook-Pro.local>
2017-01-24 22:37:49 +0200

    adding project

---
+++ src/main/java/edu/cursor/u21/ConsoleMenu.java
@@ -0,0 +1,10 @@
+package edu.cursor.u21;
+
+/**
+ * Created by alexandrmanikhin on 24.01.17.
+ */
+public class ConsoleMenu {
+
+    public void ConsoleMenu() {
+    }
+}
---
+++ src/main/java/edu/cursor/u21/Main.java
@@ -0,0 +1,11 @@
+package edu.cursor.u21;
+
+/**
+ * Created by alexandrmanikhin on 24.01.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ConsoleMenu menu = new ConsoleMenu();
+        menu.ConsoleMenu();
+    }
+}
