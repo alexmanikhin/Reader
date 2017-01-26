a0c46566592f0d6f9cc8bb4e0a07f1f8e0782f78
Oleksandr Kociuta <kocytaa@ukr.net>
2017-01-27 00:46:17 +0200

    Delete class field in U21ReaderClass, change U21ReaderMethod readFile() now it return string which contain file's Text, delete ArrayList

--- src/main/java/edu/cursor/u21/U21Reader.java
+++ src/main/java/edu/cursor/u21/U21Reader.java
@@ -2,14 +2,11 @@ package edu.cursor.u21;
 
 import lombok.Getter;
 
-import java.util.ArrayList;
-
 /**
  * Created by o.kociuta on 25.01.2017.
  */
 @Getter
 public class U21Reader {
-    private ArrayList<String> splitWords = new ArrayList<>();
     private String filePath;
 
     public U21Reader() {
--- src/main/java/edu/cursor/u21/U21ReaderMethods.java
+++ src/main/java/edu/cursor/u21/U21ReaderMethods.java
@@ -1,37 +1,20 @@
 package edu.cursor.u21;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by o.kociuta on 25.01.2017.
  */
 public class U21ReaderMethods implements U21ReaderMethodsInterface {
     @Override
-    public void lexemesAndTokensDivider(U21Reader reader) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(reader.getFilePath()));
-        String currentLine;
-        String str;
-        while ((currentLine = br.readLine()) != null) {
-            for (int i = 0; i < currentLine.length(); i++) {
-                str = "";
-                if (currentLine.charAt(i) >= (char) (48) && currentLine.charAt(i) <= (char) (57) || currentLine.charAt(i) >= (char) (65) && currentLine.charAt(i) <= (char) (90) || currentLine.charAt(i) >= (char) (97) && currentLine.charAt(i) <= (char) (122)) {
-                    while (true) {
-                        str += currentLine.charAt(i);
-                        if (i == currentLine.length() - 1 || currentLine.charAt(i + 1) >= (char) (32) && currentLine.charAt(i + 1) <= (char) (47) || currentLine.charAt(i + 1) >= (char) (58) && currentLine.charAt(i + 1) <= (char) (64) || currentLine.charAt(i + 1) >= (char) (91) && currentLine.charAt(i + 1) <= (char) (96) || currentLine.charAt(i + 1) >= (char) (123) && currentLine.charAt(i + 1) <= (char) (126)) {
-                            break;
-                        } else {
-                            i++;
-                        }
-                    }
-                } else if (currentLine.charAt(i) != (char) (32)) {
-                    str += currentLine.charAt(i);
-                } else {
-                    continue;
-                }
-                reader.getSplitWords().add(str);
-            }
+    public String readFile(U21Reader reader) throws IOException {
+        Scanner in = new Scanner(new File(reader.getFilePath()));
+        String str = "";
+        while (in.hasNextLine()) {
+            str += in.nextLine() + " ";
         }
+        return str;
     }
 }
