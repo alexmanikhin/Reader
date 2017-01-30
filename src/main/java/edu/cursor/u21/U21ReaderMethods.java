fb353a5d3e6648395cc959d34840647e00c986a3
Alexandr Manikhin <alexandrmanikhin@Alexandrs-MacBook-Pro.local>
2017-01-30 17:49:46 +0200

    Some changes from my side. ConsoleMenu and e.t.

--- src/main/java/edu/cursor/u21/U21ReaderMethods.java
+++ src/main/java/edu/cursor/u21/U21ReaderMethods.java
@@ -3,12 +3,13 @@ package edu.cursor.u21;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
 
 /**
  * Created by o.kociuta on 25.01.2017.
  */
 public class U21ReaderMethods implements U21ReaderMethodsInterface {
+    private String str;
+
     @Override
     public String readFile(U21Reader reader) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(reader.getFilePath()));
@@ -18,62 +19,42 @@ public class U21ReaderMethods implements U21ReaderMethodsInterface {
             System.out.println("File is empty");
             System.out.println("Overwrites text");
             U21Reader reader1 = new U21Reader();
-            lineCollector = readFile(reader1);
+            readFile(reader1);
         } else {
             System.out.println("File is not empty");
-        }
-        while ((currentLine = br.readLine()) != null) {
-            lineCollector += currentLine + " ";
+            while ((currentLine = br.readLine()) != null) {
+                lineCollector += currentLine + " ";
+            }
         }
         return lineCollector;
     }
 
-    @Override
-    public String[] deleteMetachAndPreposition(String str) {
-        str = " " + str + " ";
-        str = str.toLowerCase();
-        str = str.replaceAll("(`)|(~)|(!)|(@)|(#)|(\\$)|(%)|(\\^)|(&)|(\\*)|(\\()|(\\))|(-)|(_)|(=)|(\\+)|(\\[)|(\\])|" +
-                "(\\{)|(\\})|(\\|)|(')|(\")|(;)|(:)|(<)|(,)|(>)|(\\.)|(/)|(\\?)|(№)|(\\\\)", " ");
-        for (int i = 0; i < 2; i++) {
-            str = str.replaceAll("( \\S )|( an )|( on )|( in )|( at )|( to )|( the )|( up )|( under )|( over )|( since )|" +
-                    "( about )|( of )|( from )|( because )|( above )|( after )|( upon )|( off )|( for )|( out )|( into )|" +
-                    "( down )|( through )|( across )|( along )|( by )|( behind )|( front )|( under )|( among )|(between)|" +
-                    "( during )|( till )|( untill )|( within )|( ago )|( before )|( past )|( accordance )|( below )|" +
-                    "( without )|( onto )|( toward )|( away )|( near )|( beside )|( will )|( is )|( that )|( be )|" +
-                    "( can )|( must )|( any )|( no )|( not )|( this )|( and )|( which )", "  ");
-        }
-        str = str.replaceAll(" +", " ");
-        str = str.replaceFirst(" ", "");
-        return str.split(" ");
-    }
-
-    @Override
-    public String[] checkOnTheNumberOfWords(String[] str) throws IOException {
-        while (str.length < 2000) {
-            U21Reader reader = new U21Reader();
-            System.out.println("The text contains less 2000 is word");
-            System.out.println("Replay file");
-            str = checkOnTheNumberOfWords(deleteMetachAndPreposition(readFile(reader)));
-        }
-        System.out.println("The file has " + str + "words");
-        return str;
-    }
-
-    public void wordsStatistics(U21Reader reader)throws IOException{
-
-        String arrayWords[] = deleteMetachAndPreposition(readFile(reader));
-        HashMap<String, Integer> hashMap = new HashMap<>(2500, 1);
-
-        for (int i = 0; i < arrayWords.length; i++){
-            if (!hashMap.containsKey(arrayWords[i])){
-                int counter = 1;
-                hashMap.put(arrayWords[i], counter);
-                for (int j = i; j < arrayWords.length; j++) {
-                    if (0 == arrayWords[i].compareTo(arrayWords[j])) {
-                        hashMap.put(arrayWords[i], (counter++));
-                    }
-                }
+        @Override
+        public String[] deleteMatchAndPreposition(String str){
+            str = " " + str + " ";
+            str = str.toLowerCase();
+            str = str.replaceAll("(`)|(~)|(!)|(@)|(#)|(\\$)|(%)|(\\^)|(&)|(\\*)|(\\()|(\\))|(-)|(_)|(=)|(\\+)|(\\[)|(\\])|" + "(\\{)|(\\})|(\\|)|(')|(\")|(;)|(:)|(<)|(,)|(>)|(\\.)|(/)|(\\?)|(№)|(\\\\)", " ");
+            for (int i = 0; i < 2; i++) {
+                str = str.replaceAll("( \\S )|( an )|( on )|( in )|( at )|( to )|( the )|( up )|( under )|( over )|( since )|" + "( about )|( of )|( from )|( because )|( above )|( after )|( upon )|( off )|( for )|( out )|( into )|" + "( down )|( through )|( across )|( along )|( by )|( behind )|( front )|( under )|( among )|(between)|" + "( during )|( till )|( untill )|( within )|( ago )|( before )|( past )|( accordance )|( below )|" + "( without )|( onto )|( toward )|( away )|( near )|( beside )|( will )|( is )|( that )|( be )|" + "( can )|( must )|( any )|( no )|( not )|( this )|( and )|( which )", "  ");
             }
+            str = str.replaceAll(" +", " ");
+            str = str.replaceFirst(" ", "");
+            return str.split(" ");
         }
+
+        @Override
+        public String[] checkOnTheNumberOfWords(String[] str) throws IOException {
+            while (str.length < 2000) {
+                U21Reader reader = new U21Reader();
+                System.out.println("The text contains less 2000 is word");
+                System.out.println("Replay file");
+                str = checkOnTheNumberOfWords(deleteMatchAndPreposition(readFile(reader)));
+            }
+            System.out.println("The file has " + str + "words");
+            return str;
+        }
+
+
     }
-}
+
+
--- src/main/java/edu/cursor/u21/U21ReaderMethodsInterface.java
+++ src/main/java/edu/cursor/u21/U21ReaderMethodsInterface.java
@@ -7,7 +7,6 @@ import java.io.IOException;
  */
 public interface U21ReaderMethodsInterface {
     public String readFile(U21Reader reader) throws IOException;
-    public String [] deleteMetachAndPreposition(String str);
-    public String [] checkOnTheNumberOfWords(String[] str) throws IOException;
-    public void wordsStatistics(U21Reader reader) throws IOException;
+    public String[] deleteMatchAndPreposition(String str);
+    public String[] checkOnTheNumberOfWords(String[] str) throws IOException;
 }
